9fddc7c670535b7b96d79a207f552b251ee2b483
Hope Davids <hopeolawole@gmail.com>
2023-08-23 18:46:25 +0000

    applied PEP8 linting to file

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 
-""" Create a class FIFOCache that inherits from BaseCaching and is a caching system """
+""" Create a class FIFOCache that inherits from BaseCaching
+    and is a caching system.
+"""
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-    """ This class FIFOCache that inherits from BaseCaching and is a caching system """
+    """ This class FIFOCache that inherits from BaseCaching and
+        is a caching system.
+    """
 
     def __init__(self):
         super().__init__()
         self.cache_data = {}
 
     def put(self, key, item):
-        """ This method Must assign to the dictionary self.cache_data the item value for the key key. """
+        """ This method Must assign to the dictionary self.cache_data
+            the item value for the key key.
+        """
 
         if key is None or item is None:
             return
-        
+
         if key not in self.cache_data:
             self.cache_data[key] = item
             return
@@ -27,9 +34,11 @@ class FIFOCache(BaseCaching):
             print(f"DISCARD: {removed_key}")
 
         self.cache_data[key] = item
-    
+
     def get(self, key):
-        """ This method must return the value in self.cache_data linked to key """
+        """ This method must return the value in self.cache_data linked
+            to key.
+        """
         if key is None or key not in self.cache_data:
             return None
 
