d3e066c2fa749a8b4baf17803ba4beb2e69068e2
Hope Lekan Davids <hopeolawole@gmail.com>
2023-09-27 15:37:18 +0000

    redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+
+const client = await createClient()
+.on('error', (err) => {
+    console.log('Redis client not connected to the server:', err);
+})
+.on("connect", () => {
+    console.log(`Redis client connected to the server`);
+});
